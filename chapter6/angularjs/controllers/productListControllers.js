4ed562b09a06a623b71454d540b9c8a55b5e2507
DercilioFontes <derciliofontes@gmail.com>
2020-03-25 22:16:07 -0700

    Finished Chapter 6

--- chapter6/angularjs/controllers/productListControllers.js
+++ chapter6/angularjs/controllers/productListControllers.js
@@ -1,15 +1,25 @@
 angular
   .module("sportsStore")
   .constant("productListActiveClass", "btn-primary")
+  .constant("productListPageCount", 3)
   .controller("productListCtrl", function(
     $scope,
     $filter,
-    productListActiveClass
+    productListActiveClass,
+    productListPageCount
   ) {
     let selectedCategory = null;
 
+    $scope.selectedPage = 1;
+    $scope.pageSize = productListPageCount;
+
     $scope.selectCategory = function(newCategory) {
       selectedCategory = newCategory;
+      $scope.selectedPage = 1;
+    };
+
+    $scope.selectPage = function(newPage) {
+      $scope.selectedPage = newPage;
     };
 
     $scope.categoryFilterFn = function(product) {
@@ -21,5 +31,9 @@ angular
 
     $scope.getCategoryClass = function(category) {
       return selectedCategory === category ? productListActiveClass : "";
+    };
+
+    $scope.getPageClass = function(page) {
+      return $scope.selectedPage === page ? productListActiveClass : "";
     };
   });
--- chapter6/angularjs/filters/customFilters.js
+++ chapter6/angularjs/filters/customFilters.js
@@ -1,18 +1,42 @@
-angular.module("customFilters", []).filter("unique", function() {
-  return function(data, propertyName) {
-    if (angular.isArray(data) && angular.isString(propertyName)) {
-      const results = [];
-      const keys = {};
-      for (let i = 0; i < data.length; i++) {
-        const val = data[i][propertyName];
-        if (angular.isUndefined(keys[val])) {
-          keys[val] = true;
-          results.push(val);
+angular
+  .module("customFilters", [])
+  .filter("unique", function() {
+    return function(data, propertyName) {
+      if (angular.isArray(data) && angular.isString(propertyName)) {
+        const results = [];
+        const keys = {};
+        for (let i = 0; i < data.length; i++) {
+          const val = data[i][propertyName];
+          if (angular.isUndefined(keys[val])) {
+            keys[val] = true;
+            results.push(val);
+          }
         }
+        return results;
+      } else {
+        return data;
       }
-      return results;
-    } else {
-      return data;
-    }
-  };
-});
+    };
+  })
+  .filter("range", function($filter) {
+    return function(data, page, size) {
+      if (
+        angular.isArray(data) &&
+        angular.isNumber(page) &&
+        angular.isNumber(size)
+      ) {
+        const startIndex = (page - 1) * size;
+        if (data.length < startIndex) return [];
+        else return $filter("limitTo")(data.splice(startIndex), size);
+      } else return data;
+    };
+  })
+  .filter("pageCount", function() {
+    return function(data, size) {
+      if (angular.isArray(data)) {
+        const result = [];
+        for (let i = 0; i < Math.ceil(data.length / size); i++) result.push(i);
+        return result;
+      } else return data;
+    };
+  });
