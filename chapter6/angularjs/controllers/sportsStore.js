7c7ab580062842b3773d89b65f5702d91c1d4eed
DercilioFontes <derciliofontes@gmail.com>
2020-04-15 23:11:31 -0700

    Added until chapter 8 - Adding the Placeholder View

--- chapter6/angularjs/controllers/sportsStore.js
+++ chapter6/angularjs/controllers/sportsStore.js
@@ -1,11 +1,35 @@
 angular
   .module("sportsStore")
   .constant("dataUrl", "http://localhost:2403/products")
-  .controller("sportsStoreCtrl", function($scope, $http, dataUrl) {
+  .constant("orderUrl", "http://localhost:2403/orders")
+  .controller("sportsStoreCtrl", function (
+    $scope,
+    $http,
+    $location,
+    dataUrl,
+    orderUrl,
+    cart
+  ) {
     $scope.data = {};
 
     $http.get(dataUrl).then(
-      resp => ($scope.data.products = resp.data),
-      error => ($scope.data.error = error.data)
+      (resp) => ($scope.data.products = resp.data),
+      (error) => ($scope.data.error = error.data)
     );
+
+    $scope.sendOrder = function (shippingDetails) {
+      const order = angular.copy(shippingDetails);
+      order.products = cart.getProducts();
+
+      $http
+        .post(orderUrl, order)
+        .then(
+          (resp) => {
+            $scope.data.orderId = resp.data.id;
+            cart.getProducts().length = 0;
+          },
+          (error) => ($scope.data.orderError = error.data)
+        )
+        .finally(() => $location.path("/complete"));
+    };
   });
